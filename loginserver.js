d79f915a06ad006b41e63732b3b8c63535808f41
Davide Melillo <53042368+Dvixen14@users.noreply.github.com>
2021-05-08 21:17:44 +0200

    My first commit

---
+++ loginserver.js
@@ -0,0 +1,16 @@
+var port = process.env.PORT||3000;
+const express = require('express');
+let ejs = require('ejs');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.set('view-engine', 'ejs')
+
+app.get('/', (req, res) =>{
+    res.render('index.ejs', { name: 'Davide'})
+})
+
+server.listen(port, () => {
+    console.log('listening on *:3000');
+});
