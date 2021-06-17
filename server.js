a9213ce39861ee59ca7b2a894b2f619bd9ea1b48
Davide Melillo <53042368+Dvixen14@users.noreply.github.com>
2021-06-17 17:12:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
-if(process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https')
-      res.redirect(`https://${req.header('host')}${req.url}`)
-    else
-      next()
-  })
-}
+
 var port = process.env.PORT || 8000;
 const MongoClient = require("mongodb").MongoClient;
 const MongoStore = require("connect-mongo");
